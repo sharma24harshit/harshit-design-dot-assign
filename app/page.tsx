ffde7bfd075371f8596edc2848d8eb688810d46a
unknown <sharma78655533@gmail.com>
2026-03-27 18:12:55 +0530

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+       <h1>Hello world</h1>
+    </div>
+  );
+}
+
+
+{/* <div >
+<Image
+  className="dark:invert"
+  src="/next.svg"
+  alt="Next.js logo"
+  width={100}
+  height={20}
+  priority
+/>
+</div> */}
